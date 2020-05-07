8e7937fc9fafd52319c87071012045d1e5b3a10a
Alexander Kononov <kononov8214@gmail.com>
2020-05-07 17:20:08 +0300

    allmost done

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,15 @@
 $(document).ready(function(){
     $('.skills__btn--active').click(function(){
         $('.skills__web,.skills__front').toggle( function(){
-            console.log("toggle completed");
         });
-    }); 
-
+    });
+    $('.scroll a[href^="#"]').click( function(){
+    var scroll_el = $(this).attr('href');
+    if ($(scroll_el).length != 0) {
+        $('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 800);
+        $('html, body').animate({ scrollBottom: $(scroll_el).offset().bottom }, 800);
+        // $('')
+    }
+    return false;
+    });
 });

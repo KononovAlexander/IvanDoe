ef446daa568ee26c6d93599a97ce515d243f7687
Alexander Kononov <kononov8214@gmail.com>
2020-05-20 09:40:34 +0300

    fixed SKILLS

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,13 @@
 $(document).ready(function(){
-    $('.skills__btn--active').click(function(){
-        $('.skills__web,.skills__front').toggle( function(){
-        });
-    });
+    // $('.skills__btn--active').click(function(){
+    //     $('.skills__web,.skills__front').toggle( function(){
+    //     });
+    // });
     $('.scroll a[href^="#"]').click( function(){
     var scroll_el = $(this).attr('href');
     if ($(scroll_el).length != 0) {
         $('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 800);
         $('html, body').animate({ scrollBottom: $(scroll_el).offset().bottom }, 800);
-        // $('')
     }
     return false;
     });
